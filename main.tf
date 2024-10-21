716146b60b1301ffcceb55c2339d3d20ab67f139
mandy2666 <146002294+mandy2666@users.noreply.github.com>
2024-10-21 14:21:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
 terraform {
   required_providers {
-    linux = {
-      source = "example/linux"
-      version = "1.0.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"  # Specify the version you want
     }
   }
 }
 
-provider "linux" {
-  # Replace with your actual configuration to connect to the Ubuntu machine
-  host     = "<IP_ADDRESS>"
-  user     = "<USERNAME>"
-  password = "<PASSWORD>"
+provider "aws" {
+  region = "us-west-2"  # Specify the region you want
 }
 
-resource "linux_instance" "runner" {
-  name      = "ubuntu-github-runner"
-  os        = "ubuntu"
+resource "aws_instance" "github_runner" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with an Ubuntu AMI in your region
+  instance_type = "t2.micro"  # Choose the instance type
 }
